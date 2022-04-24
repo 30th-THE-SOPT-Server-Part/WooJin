1382bddd77bf4915e137175e7f17691c7488a8a1
Lee Woojin <dhkep03@gmail.com>
2022-04-24 15:44:02 +0900

    level 2 ~ing

---
+++ 2nd_seminar/level2/assignment/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', require('./api'));
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hi! This is my first express server. My name is Woojin.');
+});
+
+app.listen('8000', () => {
+    console.log(`
+    #############################################x
+        🛡️ Server listening on port: 8000 🛡️
+    #############################################    
+    `)
+})
+
+
