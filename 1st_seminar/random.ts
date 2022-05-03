266e55425d8ee1cc8214cadf627b174b23f9e6e4
Lee Woojin <dhkep03@gmail.com>
2022-05-03 23:27:27 +0900

    1주차 최종

--- 1st_seminar/random.ts
+++ 1st_seminar/random.ts
@@ -1,17 +1,4 @@
-interface Member {
-    name: string;
-    group: string;
-}
-
-interface Dinner {
-    member: Member[];
-    shuffle: {
-        (array: Member[]): Member[];
-    };
-    organize: {
-        (array: Member[]): void;
-    };    
-}
+import { Dinner } from './interface/Dinner';
 
 const dinner: Dinner = {
     member: [
@@ -49,94 +36,14 @@ const dinner: Dinner = {
         const ob = array.find((member) => member.group === 'ob');
         const yb = array.find((member) => member.group === 'yb');
         console.log(yb)
-        // if (ob && yb) {
-        //     const dinnerMember: string[] = [ob.name, yb.name];
-        //     console.log(
-        //     `오늘의 저녁 식사 멤버는 ${dinnerMember[0]}, ${dinnerMember[1]}`
-        //     );        
-        // // console.log(`오늘의 저녁 식사 멤버는 ${ob.name}, ${yb.name}`)
-        // }
-        const dinnerMember: string[] = [ob.name, yb.name];
-        console.log(
-        `오늘의 저녁 식사 멤버는 ${dinnerMember[0]}, ${dinnerMember[1]}`
-        );        
-        // console.log(`오늘의 저녁 식사 멤버는 ${ob.name}, ${yb.name}`)      
-    }
-    
+        if (ob && yb) {
+            const dinnerMember: string[] = [ob.name, yb.name];
+            console.log(
+            `오늘의 저녁 식사 멤버는 ${dinnerMember[0]}, ${dinnerMember[1]}`
+            );        
+ 
+        };
+    },
 };
 
 dinner.organize(dinner.member);
-
-
-
-
-
-
-
-
-
-
-/**
- * 이하 현지 코드
- */
-
-
-
-
-
-// interface Member {
-//     name: string;
-//     group: string;
-// };
-
-// interface Dinner {
-//   member: Member[];
-//   shuffle(members: Member[]): Member[];
-//   organize(members: Member[]): void;
-// };
-
-
-// const dinner: Dinner = {
-//   member: [
-//     {
-//       name: '채정아',
-//       group: 'ob',
-//     },
-//     {
-//       name: '김동재',
-//       group: 'yb',
-//     },
-//     {
-//       name: '강민재',
-//       group: 'yb',
-//     },
-//     {
-//       name: '김루희',
-//       group: 'ob',
-//     },
-//     {
-//       name: '박진수',
-//       group: 'ob',
-//     },
-//   ],
-//   shuffle(array) {
-//     array.sort(() => Math.random() - 0.5);
-//     return array;
-//   },
-//   organize(array) {
-//     this.shuffle(array);
-
-//     const ob = array.find((mem) => mem.group === 'ob');
-//     const yb = array.find((mem) => mem.group === 'yb');
-
-
-//     if (ob && yb) {
-//       const dinnerMember: string[] = [ob.name, yb.name];
-//       console.log(
-//         `오늘의 저녁 식사 멤버는 ${dinnerMember[0]}, ${dinnerMember[1]}`
-//       );
-//     }
-//   },
-// };
-
-// dinner.organize(dinner.member);
