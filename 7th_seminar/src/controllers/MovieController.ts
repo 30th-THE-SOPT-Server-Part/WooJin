0b8ae80e73fbf778824c832858d615443d166b0f
Woojin Lee <dhkep03@gmail.com>
2022-06-14 21:11:03 +0900

    [ADD] 7th seminar level1

---
+++ 7th_seminar/src/controllers/MovieController.ts
@@ -0,0 +1,143 @@
+import express, { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import statusCode from "../modules/statusCode";
+import message from "../modules/responseMessage";
+import util from "../modules/util";
+import { MovieCreateDto } from "../interfaces/movie/MovieCreateDto";
+import MovieService from "../services/MovieService";
+import { MovieCommentCreateDto } from "../interfaces/movie/MovieCommentCreateDto";
+import { MovieCommentUpdateDto } from "../interfaces/movie/MovieCommentUpdateDto";
+
+/**
+ * @route POST /movie
+ * @description Create Movie
+ * @access Public
+ */
+const createMovie = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        console.log(error);
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+    }
+
+    const movieCreateDto: MovieCreateDto = req.body;
+
+    try {
+        const data = await MovieService.createMovie(movieCreateDto);
+
+        res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_MOVIE_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+}
+
+/**
+ * @route POST /movie/:movieId/comment
+ * @description Create Movie Comment
+ * @access Public
+ */
+const createMovieComment = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        console.log(error);
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+    }
+    
+    const movieCommentCreateDto: MovieCommentCreateDto = req.body;
+    const { movieId } = req.params;
+
+    try {
+        const data = await MovieService.createMovieComment(movieId, movieCommentCreateDto);
+        if (!data) return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+
+        res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_MOVIE_COMMENT_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+}
+
+/**
+ * @route GET /movie/:movieId
+ * @description Get Movie
+ * @access Public
+ */
+const getMovie = async (req: Request, res: Response): Promise<Response | void> => {
+    const { movieId } = req.params;
+
+    try {
+        const data = await MovieService.getMovie(movieId);
+        if (!data) return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.READ_MOVIE_SUCCESS, data));      
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+}
+
+/**
+ * @route PUT /movie/:movieId/comments/:commentId
+ * @description Update Movie Comment
+ * @access Public
+ */
+const updateMovieComment = async (req: Request, res: Response): Promise<Response | void> => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        console.log(error);
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+    }
+
+    const commentUpdateDto: MovieCommentUpdateDto = req.body;
+    const { movieId, commentId } = req.params;
+    const userId = req.body.user.id;
+
+    try {
+        const data = await MovieService.updateMovieComment(movieId, commentId, userId, commentUpdateDto);
+
+        if (!data) return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.UPDATE_MOVIE_COMMENT_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+}
+
+/**
+ * @route GET /movie?search=
+ * @desc Update Movie Comment
+ * @access Public
+ */
+ const getMoviesBySearch = async (req: Request, res: Response) => {
+    const { search, option } = req.query;
+
+    const isOptionType = (option: string): option is MovieOptionType => {
+        return ["title", "director", "title_director"].indexOf(option) !== -1;
+    }
+
+    if (!isOptionType(option as string)) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+    }
+
+    const page: number = Number(req.query.page || 1);
+
+    try {
+        const data = await MovieService.getMoviesBySearch(search as string, option as MovieOptionType, page);
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.SEARCH_MOVIE_SUCCESS, data));
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+}
+
+
+export default {
+    createMovie,
+    createMovieComment,
+    getMovie,
+    updateMovieComment,
+    getMoviesBySearch
+}
---
+++ 7th_seminar/src/controllers/index.ts
@@ -0,0 +1,10 @@
+import UserController from "./UserController"
+import MovieController from "./MovieController"
+import ReviewController from "./ReviewController"
+
+// controller index file
+export {
+   UserController,
+   MovieController,
+   ReviewController,
+}
